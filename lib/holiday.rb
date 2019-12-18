969a38ca134d2a706410c689fd13cac0d9d1525d
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-18 19:05:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,8 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash.collect do |
-
+  holiday_hash[:winter].collect do |holiday, supply|
+    puts supply
 end
 
 def all_supplies_in_holidays(holiday_hash)
