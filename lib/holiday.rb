f208dbc59c7e36941798f211a1dcc2dbdff1da09
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-18 19:29:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,9 +49,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
   data.each do |holiday, supplies|
-    holiday.to_s.split(" ").collect do |caps|
+    holiday_string = holiday.to_s.split("_").collect do |caps|
       caps.capitalize!
-    }"
+    
     
     
   # iterate through holiday_hash and print items such that your readout resembles:
