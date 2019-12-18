38efbf185b01a57ceff2ca4b48565fbb052e7968
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-18 18:43:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
    supply << decoration
       end
 end
-add_supply_to_winter_holidays(holiday_hash, "Baloons")
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
