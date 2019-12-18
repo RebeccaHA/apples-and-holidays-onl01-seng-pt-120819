e564d56a849a95d1bc95655f3a933039f7032a73
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-18 18:31:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,11 +21,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-  
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |supplies|
+      if season == :winter 
+        supplies << "Balloons"
+      end
+     end   
+  end
 end
 
 
